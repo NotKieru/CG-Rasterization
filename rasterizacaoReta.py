35391653df315e109a53dcea31d696f57460206f
NotKieru <isequielnascimento@gmail.com>
2024-07-27 17:11:36 -0300

    Feat: Adicionando testes estáticos no código para geração das retas.

--- rasterizacaoReta.py
+++ rasterizacaoReta.py
@@ -43,3 +43,25 @@ def rasterizar_reta(x0, y0, x1, y1, resolucao):
         
     return imagem
 
+#OBS: AJEITAR ISSO DAQUI DEPOIS< AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+# Exemplo de criação de 5 segmentos de retas e 4 resoluções. 
+resolutions = [100, 200, 300, 400]
+lines = [
+    (10, 10, 90, 90),
+    (20, 10, 70, 80),
+    (30, 30, 90, 10),
+    (40, 50, 90, 50),
+    (10, 20, 10, 80)
+]
+
+#pinta nas resoluções
+for resolution in resolutions:
+    plt.figure(figsize=(10, 10))
+    for i, (x0, y0, x1, y1) in enumerate(lines):
+        image = rasterizar_reta(x0, y0, x1, y1, resolution)
+        plt.subplot(2, 3, i+1)
+        plt.imshow(image, cmap='gray', vmin=0, vmax=1)
+        plt.title(f'Line {i+1}')
+        plt.axis('off')
+    plt.suptitle(f'Resolution: {resolution}')
+    plt.show()
